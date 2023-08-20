c55d4b9450c18c3f68717b167dbe5624825f47bf
Sebastian Krzyżak <sebastiankontakt7@gmail.com>
2023-08-20 20:43:56 +0200

    Fixing build script

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "lib/reader/reader.hpp"
 
 int main(int argc, char *argv[]) {
-  std::cout << argv[0] << std::endl;
-  std::cout << argc << std::endl;
+  std::vector<std::string> input = {argv[0], argv[1]};
+  std::unique_ptr<cman::Reader> rd = std::make_unique<cman::Reader>();
+  std::unique_ptr<cman::Command> cmd = std::make_unique<cman::Invalid>();
+
+  cmd.reset(rd->interpret(input, argc));
+  std::cout << cmd->execute() << std::endl;
+  cmd.reset(nullptr);
+  rd.reset(nullptr);
 
   return 0;
 }
